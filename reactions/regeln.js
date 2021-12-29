b96876499d29f5bec360a99e2db2218423b21deb
HackerLoad <hackerloade@gmail.com>
2021-12-29 15:27:02 +0100

    update

--- reactions/regeln.js
+++ reactions/regeln.js
@@ -16,17 +16,19 @@ module.exports = {
         .setDescription('Hier sind die Regeln des Community Servers von patrickf1gaming')
         .setThumbnail('https://i.imgur.com/lIhUgVv.png')
         .addFields(
-            { name: '► §1 - Allgemeines', value: 'Die Regeln gelten als akzeptiert, sobald man auf die Nachricht unterhalb der Regeln reagiert. Dies gilt auch für Freunde und Besucher. Verstöße gegen das Regelwerk werden mit Strafen geahndet.' },
-            { name: '► §2 - Verhalten', value: 'Beleidigung, sexuelle Belästigung, Diskriminierung, Hassrede, Rassismus, Hetzerei, Trolling und ähnlich schädliches Verhalten gegenüber einem anderen wird Konsequenzen nach sich ziehen. "Backseat-Gaming" ist in jeder Hinsicht unerwünscht.' },
-            { name: '► §3 - Inhalte', value: 'Inhalte in Nicknamen, Profilbildern, im Chat oder darin befindliche Links dürfen nicht pornografisch, gewaltverherrlichend, rassistisch, beleidigend oder illegal sein oder gar die Privatsphäre verletzen.' },
-            { name: '► §4 - Inhalte', value: 'Alle Geräusche, die eine Konversation unmöglich machen oder erschweren gelten als Störung. Technische Probleme oder Probleme wie Echos, Rückkopplungen, Rauschen, wiederholte laute und obszöne Geräusche, sowie hörbares Atmen und Essen sind vermeidbare Störquellen.' },
-            { name: '► §5 - Streaming & Aufnahme', value: 'Die unangekündigte Aufnahme innerhalb eines Voice-Channels ist verboten. Die Ausnahme gilt, wenn alle im Channel befindlichen Personen der Aufnahme zugestimmt haben.' },
-            { name: '► §6 - Werbung', value: 'Werbung ist ohne Absprache mit der Serverleitung verboten. Als Werbung gelten jegliche Links, Videos, Bilder, Texte, Uploads, welche ein werbetechnisches Nutzen verfolgen.' },
-            { name: '► §7 - Rechte', value: 'Das Erbetteln von Rechten ist untersagt. Die Vergabe von Serverrollen unterliegt dem Serverteam und kann je nach Bedarf geändert werden.' },
-            { name: '► §8 - Weisungsrecht', value: 'Die Mitglieder des Serverteams haben auf dem Server volles Weisungsrecht. Den Anweisungen muss Folge geleistet werden.' },
-            { name: '► §9 - letzes Wort', value: 'Die Mitglieder des Serverteams haben auf dem Server das letzte Wort.' },
-            { name: '► §10 - Weitere Rechte des Serverteams', value: 'Die Mitglieder des Serverteams haben auf dem Server volles Kontrollrecht. Sie können unangemeldet in einen auch geschützten Channel wechseln oder einzelne Nutzer verschieben, falls die Notwendigkeit dazu gesehen wird. Dieses Recht wird nur unter besonderen Umständen gebraucht und niemals mit der Absicht die Privatsphäre zu stören.' },
-            { name: '► §11 - Änderungsvorbehalt', value: 'Wir behalten uns stetig vor diese Regeln zu ändern und/oder sie in einer anderen Form anzuwenden.' },
+            { name: '§1 Inhalte:', value: 'Inhalte in Namen, Profilbildern, Benutzerinformationen, Stati, Nachrichten und darin enthaltende Links dürfen nicht beleidigend, pornografisch, gewaltverherrlichend, rassistisch, beleidigend oder illegal sein.' },
+            { name: '§2 Verhalten:', value: 'Beleidigung, sexuelle Belästigung, Diskriminierung, Hassrede, Rassismus, Hetzerei, Trolling und ähnlich schädliches Verhalten gegenüber einem anderen sind nicht gestattet.' },
+            { name: '§3 Spam:', value: 'Das Spammen in Form von Links, denselben Texten oder Emojis wird mit Verwarnungen bestraft und kann zum Bann führen.' },
+            { name: '§4 Betrug:', value: '(Identitäts-)Betrug ist untersagt und wird mit einem Bann bestraft.' },
+            { name: '§5 Werbung:', value: 'Werbung ist ohne Absprache mit der Serverleitung verboten. Als Werbung gelten jegliche Links, Videos, Bilder, Texte, Uploads, welche ein werbetechnisches Nutzen verfolgen. Dies gilt auch, wenn man einzelnen Mitglieder des Servers werbetechnische Nachrichten per DM abschickt.' },
+            { name: '§6 Tickte-Tool:', value: 'Fragen, Anmerkungen, Entbannungsanträge, Beschwerden, etc. gehören ins Ticket-Tool oder den Community Chat, nicht in die DMs der Admins, Moderatoren. Wer das Ticket-Tool missbraucht, wird mit einem Bann bestraft.' },
+            { name: '§7 Rechte:', value: 'Das Erbetteln von Rechten ist untersagt. Die Vergabe von Serverrollen unterliegt dem Serverteam und kann je nach Bedarf geändert werden.' },
+            { name: '§8 Streaming & Aufnahmen:', value: 'Die unangekündigte Aufnahme innerhalb eines Voice-Channels ist verboten. Die Ausnahme gilt, wenn alle im Channel befindlichen Personen der Aufnahme zugestimmt haben.' },
+            { name: '§9 Bann Umgehung:', value: 'Die Umgehung eines Bannes, indem man z.B. einen neuen Account erstellt, ist verboten und kann mit einer Verlängerung des Banns oder permanenten Bann bestraft werden.' },
+            { name: '§10 Streamen von Werken:', value: 'Das Streamen von urheberrechtlich geschützten Werken (Filme, Serien, etc.) ohne das Einverständnis des Rechteinhabers ist verboten!' },
+            { name: '§11 Rechte des Serverteams:', value: 'Die Mitglieder des Serverteams haben auf dem Server volles Kontrollrecht.'},
+            { name: '§12 Änderungsvorbehalt:', value: 'Wir behalten uns stetig vor, diese Regeln zu ändern und/oder sie in einer anderen Form anzuwenden.' },
+            { name: 'Weiteres:', value: 'Auf diesem Server gelten weiterhin die Nutzungsbedingungen und Community-Richtlinien von Discord. \n Nutzungsbedingungen: https://discordapp.com/terms \n Community-Richtlinien: https://discordapp.com/guidelines'},
         )
         .setFooter('©HackerLoad', 'https://imgur.com/i0GTz5e.png');
 
